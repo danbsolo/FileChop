a0c3299630110d29b1ff3335151702a33cf71606
Daniel Solomon <daniel.solomon@pc.gc.ca>
2025-06-12 13:52:21 -0400

    Minute changes

--- defs.py
+++ defs.py
@@ -4,14 +4,14 @@ from procedureClass import *
 
 RESULTS_DIRECTORY = "File-Chop-Results"
 
-EXCEED_CHARACTER_LIMIT_SUGGESTER = "File Rename Suggester (AI)"
+FILE_RENAME_SUGGESTER = "File Rename Suggester (AI)"
 FILE_NINJA_MODIFIER = "File-Ninja Modifier"
 TYPO_FIXER = "Typo Fixer (AI)"
 
 
 AI_PROCEDURES = {
-    EXCEED_CHARACTER_LIMIT_SUGGESTER: Procedure(
-        EXCEED_CHARACTER_LIMIT_SUGGESTER, 
+    FILE_RENAME_SUGGESTER: Procedure(
+        FILE_RENAME_SUGGESTER, 
         exceedCharacterLimitSuggesterFunction, 
         "pcFileRenamerPrompt.txt"),
     
@@ -26,7 +26,7 @@ AI_PROCEDURES = {
 }
 
 AI_PROCEDURES_DISPLAY = [
+    FILE_RENAME_SUGGESTER,
     FILE_NINJA_MODIFIER,
     TYPO_FIXER,
-    EXCEED_CHARACTER_LIMIT_SUGGESTER,
 ]
